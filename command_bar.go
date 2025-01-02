10cc18b2148363f0a95c87667baea5ff9daa2337
Zdzisław Goik <z.goik@rynekpierwotny.pl>
2025-01-02 13:59:51 +0100

    Add basic command bar logic with logging

--- command_bar.go
+++ command_bar.go
@@ -1,12 +1,48 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type CommardBarState int
+
+const (
+	CommardBarStateNormal  CommardBarState = iota
+	CommandBarStateCommand                 // special state where we're getting a command input
+	CommardBarStateEdit
+	CommardBarStateVisual
+)
+
+var CommandBarStateName = map[CommardBarState]string{
+	CommardBarStateNormal: "Normal",
+	CommardBarStateEdit:   "Edit",
+	CommardBarStateVisual: "Visual",
+}
+
+func (s CommardBarState) String() string {
+	return CommandBarStateName[s]
+}
 
 type CommandBarModel struct {
+	// state has dual purpose
+	// for one, it'll indicate what's the state of the editor
+	// and on which line are we
+	// but also it'll indicate if the command bar is selected
+	// if we're in CommandBarStateCommand state, we're selected
+	// and we're hogging all the input
+	// until we are either put out of that state
+	// or the command is submitted
+	state       CommardBarState
+	editor_line [2]int
 }
 
 func NewCommandBar() CommandBarModel {
-	return CommandBarModel{}
+	return CommandBarModel{
+		state:       CommardBarStateNormal,
+		editor_line: [2]int{0, 0},
+	}
 }
 
 func (m CommandBarModel) Init() tea.Cmd {
@@ -23,7 +59,24 @@ func (m CommandBarModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q":
-			return m, tea.Quit
+			if m.state == CommardBarStateNormal {
+				return m, tea.Quit
+			}
+		case ":":
+			if m.state == CommardBarStateNormal {
+				m.SetState(CommandBarStateCommand)
+				return m, nil
+			}
+		case "v":
+			if m.state == CommardBarStateNormal {
+				m.SetState(CommardBarStateVisual)
+				return m, nil
+			}
+		case "esc":
+			if m.state != CommardBarStateNormal {
+				m.SetState(CommardBarStateNormal)
+				return m, nil
+			}
 		}
 	}
 
@@ -31,5 +84,13 @@ func (m CommandBarModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m CommandBarModel) View() string {
-	return ""
+	return fmt.Sprintf("commandBar state: %s", m.state)
+}
+
+func (m *CommandBarModel) SetState(state CommardBarState) {
+	m.state = state
+}
+
+func (m CommandBarModel) GetState() CommardBarState {
+	return m.state
 }
--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/davecgh/go-spew/spew"
 )
 
 type model struct {
+	logs          io.Writer
 	current_route string
 	views         map[string]tea.Model
 	commannd_bar  CommandBarModel
 }
 
-func newInitialModel() model {
+func newInitialModel(logs_file io.Writer) model {
 	views := make(map[string]tea.Model)
 	views["/"] = NewLandingPage()
 
 	return model{
+		logs:          logs_file,
 		current_route: "/",
 		views:         views,
 		commannd_bar:  NewCommandBar(),
@@ -29,6 +33,9 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if m.logs != nil {
+		spew.Fdump(m.logs, msg)
+	}
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
@@ -60,7 +67,17 @@ func (m model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(newInitialModel())
+	var dump *os.File
+
+	if _, ok := os.LookupEnv("GODIFF_DEBUG"); ok {
+		var err error
+		dump, err = os.OpenFile("messages.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
+		if err != nil {
+			os.Exit(1)
+		}
+	}
+
+	p := tea.NewProgram(newInitialModel(dump))
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Something went wrong while trying to run the program: %v \n", err)
 		os.Exit(1)
